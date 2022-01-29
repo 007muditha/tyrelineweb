17bcba46a2e3cced019f7c356be2c083f668ff01
Muditha <007mudithaperera@gmail.com>
2022-01-29 16:23:47 +0530

    adding routes

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nav = () => {
+    return(
+        <nav>
+            <h3>Logo</h3>
+            <li>About</li>
+            <li>Contact</li>
+        </nav>
+    )
+}
